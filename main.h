bb43c48c22176a361023276cb1f7c1e9c2ced25d
Dennis Murage <dmurage140@gmail.com>
2022-11-08 11:59:49 +0000

    Added header file main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+char *_strcmp(char *s1, char *s2);
+#endif
