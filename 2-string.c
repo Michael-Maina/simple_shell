d150b7a59db53e6f341b2b3cc6fc3d147542676e
Michael-Maina <michaelmaina02@gmail.com>
2022-11-11 12:18:12 +0300

    New function added

--- 2-string.c
+++ 2-string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 *_strncmp - compares two strings
 *
@@ -8,6 +9,7 @@
 *
 *Return: difference between the strings
 */
+
 int _strncmp(char *s1, char *s2, int n)
 {
 	int i = 0;
@@ -21,4 +23,33 @@ int _strncmp(char *s1, char *s2, int n)
 			break;
 	}
 	return (*s1 - *s2);
+}
+
+/**
+*_strdup - duplicates a string
+*
+*@str: string parameter
+*
+*Return: pointer to a duplicate of str
+*/
+
+char *_strdup(char *str)
+{
+	char *s;
+	size_t i = 0, len = 0;
+
+	len = _strlen(str);
+
+	s = malloc(sizeof(char *) * (len + 1));
+
+	if (s == NULL)
+		return (NULL);
+
+	while(i <= len)
+	{
+		s[i] = str[i];
+		i++;
+	}
+
+	return (s);
 }
--- main.h
+++ main.h
@@ -11,12 +11,16 @@
 
 extern char **environ;
 
+/* String manipulation functions */
+
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
 char *_strncpy(char *dest, char *src, int n);
 int _strlen(char *s);
 int _strncmp(char *s1, char *s2, int n);
+char *_strdup(char *str);
+char *_strtok(char *str, const char *delim);
 
 char *_getenv(char *name);
 void path_finder(char **cmd);
