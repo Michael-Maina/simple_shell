77569aa20407afd820f6a8e06ae0ea25651f3c48
Michael-Maina <michaelmaina02@gmail.com>
2022-11-16 00:20:40 -0800

    setenv function added

---
+++ 3-string.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * *_memcpy - copies memory area
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
+
+#include "main.h"
+
+/**
+ * *_memset - fills memory with a constant byte
+ * @s: pointer to string s
+ * @b: constant byte
+ * @n: number of bytes of memory
+ * Return: pointer to s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
